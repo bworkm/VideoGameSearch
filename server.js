9c0b8e1523c765560c3e63966f8b593c20118f19
Brian Workman <bworkm@outlook.com>
2017-01-23 11:33:01 -0800

    Created base file structure

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const pg = require('pg');
+const express = require('express');
+const bodyParser = require('body-parser');
+const requestProxy = require('express-request-proxy');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const conString = process.env.DATABASE_URL || 'postgres://localhost:5432';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public'));
+//***************************************************
+
+
+//***************************************************
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
