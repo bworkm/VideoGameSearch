7611da551a1765442b77ee01f7ea104c94c442c5
0blu3 <braelynn.luedtke@gmail.com>
2017-01-26 12:14:43 -0800

    added images

--- public/scripts/view/indexView.js
+++ public/scripts/view/indexView.js
@@ -3,44 +3,50 @@
   const indexView ={};
 
   indexView.initIndexPage = () => {
-    let template = Handlebars.compile($('#top-games-template').html()); //eslint-disable-line
+    let template = Handlebars.compile($('#top-games-template').html());
+
+    // filter to 5 games
 
     $('#index').html('');
-    $('#index').append(Game.allGames.filter(function(e){ //eslint-disable-line
+    $('#index').append(Game.allGames.filter(function(e){
       if (e.rank < 6 && e.rank > 0) {
         return true;
       }
     })
     .sort((a,b) => {return a.rank - b.rank})
     .map(template));
+
+    $('#display-all').on('click', displayButton)
+
+    }
+
+  displayButton = () => {
+    let template = Handlebars.compile($('#top-games-template').html());
+
+    $('#index').append(Game.allGames.filter(function(e){
+      if (e.rank < 0 || e.rank > 13) {
+        return true;
+      }
+    })
+    .map(template));
+
   }
 
-  //hero image slider code///////////////
   var sliderMain = function() {
-    $('#hero .flexslider').flexslider({
-      animation: 'fade',
-      slideshowSpeed: 3000
-    });
 
-    $('#hero .flexslider .slides > li').css('height', $(window).height());
-    $(window).resize(function(){
-      $('#hero .flexslider .slides > li').css('height', $(window).height());
-    });
+	  	$('#hero .flexslider').flexslider({
+			animation: "fade",
+			slideshowSpeed: 3000
+	  	});
 
-  };
+	  	$('#hero .flexslider .slides > li').css('height', $(window).height());
+	  	$(window).resize(function(){
+	  		$('#hero .flexslider .slides > li').css('height', $(window).height());
+	  	});
 
-	// var sliderSayings = function() {
-	// 	$('#fh5co-sayings .flexslider').flexslider({
-	// 		animation: "slide",
-	// 		slideshowSpeed: 3000,
-	// 		directionNav: false,
-	// 		controlNav: true,
-	// 		smoothHeight: true,
-	// 		reverse: true
-	//   	});
-	// }
+	};
 
   sliderMain();
-  module.indexView= indexView;
+  module.indexView = indexView;
 
 })(window);
